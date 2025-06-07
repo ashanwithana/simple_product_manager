aa7269a2311e4eb3546257a70ae031e6392dc2b4
ashanwithana <ashanwithana4@gmail.com>
2025-06-07 12:54:06 +0530

    Initialize project with package.json and essential dependencies

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(3000, () => {
+    connectDB()
+    console.log("Server started at http://localhost:3000")
+})
+
